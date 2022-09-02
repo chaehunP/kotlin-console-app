ad9c6bedd64303a456664be98b4878d6e80052b7
chaehunPark <chaehun.dev@gmail.com>
2022-09-02 15:46:35 +0900

    실습 1. 인사말과 사용자에게 입력 받은 정보 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("안녕하세요, Shoppi 에 오신 것을 환영합니다!")
+    println("쇼핑을 계속 하시려면 이름을 입력해주세요 :) ")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val name = readLine()
+    println("""
+        감사합니다. 반갑습니다. $name 님
+        원하시는 카테고리를 입력해주세요.
+        ***==============================***
+    """.trimIndent())  // .trimIndent()는 문자열 출력 시 자동으로 모든 입력 라인의 공통 최소 들여쓰기를 감지하고 모든 라인에서 그만큼 제거하는 기능
+                       // 다시 말해 현재 코드에 작성된 문자열은 앞에 띄어쓰기가 되어있는데 trimIndent()를 사용하지 않으면 출력시 문자열 앞이 띄어진 채로 출력됨
 }
