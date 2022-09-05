6a27d6363639f1b93690787fc561fc2b861fa4c3
chaehunPark <chaehun.dev@gmail.com>
2022-09-05 17:04:17 +0900

    실습 3-2. Home 화면과 Category 화면으로 분리

---
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -0,0 +1,35 @@
+package screen
+
+class ShoppingCategory {
+    fun showCategories() {
+        val categories = arrayOf("패션", "전자기기", "반려동물 용품")
+        for (category in categories) {
+            print("          ")
+            println(category)
+        }
+        println("***=============================***")
+        println("==> 장바구니로 이동하시려면 #을 입력해주세요")
+
+        var selectedCategory = readLine()
+        while (selectedCategory.isNullOrBlank()) {
+            println("유효한 값을 입력해주세요")
+            selectedCategory = readLine()
+        }
+
+        if (selectedCategory == "#") {
+            //TODO 1. 장바구니 이동
+        }else {
+            if(categories.contains(selectedCategory)) {
+                //TODO 2. 카테고리 상품 목록 보여주기
+            } else {
+                showErrorMesage(selectedCategory)
+            }
+        }
+    }
+
+    private fun showErrorMesage(selectedCategory: String?) {
+        println("[$selectedCategory] : 존재하지 않는 카테고리입니다. 다시 입력해주세요.")
+        println("***=============================***")
+        showCategories()
+    }
+}
--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -24,25 +24,8 @@ class ShoppingHome {
     }
 
     private fun showCategories() {
-        val categories = arrayOf("패션", "전자기기", "반려동물 용품")
-        for (category in categories) {
-            print("          ")
-            println(category)
-        }
-        println("***=============================***")
-        println("==> 장바구니로 이동하시려면 #을 입력해주세요")
+       val shoppingCategory = ShoppingCategory()  // 쇼핑카테고리의 인스턴스 생성
+        shoppingCategory.showCategories()
 
-        var selectedCategory = readLine()
-        while (selectedCategory.isNullOrBlank()) {
-            println("유효한 값을 입력해주세요")
-            selectedCategory = readLine()
-        }
-
-        if (selectedCategory == "#") {
-            //TODO 1. 장바구니 이동
-        } else {
-            //TODO 2. 카테고리 상품 목록 보여주기
-            //TODO 3. 카테고리 목록에 없는 입력 값에 대한 처리
-        }
     }
 }
